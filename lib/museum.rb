f2f54be219508e722e579e36478cef0a0ff2fd51
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-19 10:01:40 -0700

    Test Musuem can show patrons by exhibit interest and add patrons_by_exhibit_interests method

--- lib/museum.rb
+++ lib/museum.rb
@@ -24,4 +24,17 @@ class Museum
   def admit(patron)
     @patrons << patron
   end
+
+  def patrons_by_exhibit_interest
+    patrons_per_exhibit = {}
+    @exhibits.each {|exhibit| patrons_per_exhibit[exhibit] = []}
+    @patrons.each do |patron|
+      patron.interests.each do |interest|
+        recommend_exhibits(patron).each do |exhibit|
+          patrons_per_exhibit[exhibit] << patron if exhibit.name == interest
+        end
+      end
+    end
+    patrons_per_exhibit
+  end
 end
