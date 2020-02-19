e46db2a4d3bacb0f9aa7058d0ac06ea3317361f5
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-19 09:28:37 -0700

    Add recommend_exhibits method to Musuem calss

--- lib/museum.rb
+++ lib/museum.rb
@@ -5,4 +5,18 @@ class Museum
     @name = name
     @exhibits = []
   end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    recommend_exhibits_for_patron = []
+    @exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        recommend_exhibits_for_patron << exhibit if exhibit.name == interest
+      end
+    end
+    recommend_exhibits_for_patron
+  end
 end
