8ee25b9981118be153e46ecd89bc788d671da85f
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-19 09:28:16 -0700

    Test Musuem can recommend exhibits per patron's interests

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,6 +7,11 @@ require './lib/museum'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   def test_it_exists
@@ -17,49 +22,24 @@ class MuseumTest < Minitest::Test
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
   end
+
+  def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
 end
-
-# Use TDD to create a `Museum` class that responds to the following interaction pattern. For the `recommend_exhibits` method, the `Museum` should recommend exhibits that match a `Patron`'s interests.
-
-# pry(main)> dmns.name
-# # => "Denver Museum of Nature and Science"
-#
-# pry(main)> dmns.exhibits
-# # => []
-#
-# pry(main)> gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-# # => #<Exhibit:0x00007fb400bbcdd8...>
-#
-# pry(main)> dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-# # => #<Exhibit:0x00007fb400b851f8...>
-#
-# pry(main)> imax = Exhibit.new({name: "IMAX",cost: 15})
-# # => #<Exhibit:0x00007fb400acc590...>
-#
-# pry(main)> dmns.add_exhibit(gems_and_minerals)
-#
-# pry(main)> dmns.add_exhibit(dead_sea_scrolls)
-#
-# pry(main)> dmns.add_exhibit(imax)
-#
-# pry(main)> dmns.exhibits
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>, #<Exhibit:0x00007fb400acc590...>]
-#
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fb400a51cc8...>
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_2 = Patron.new("Sally", 20)
-# # => #<Patron:0x00007fb400036338...>
-#
-# pry(main)> patron_2.add_interest("IMAX")
-#
-# pry(main)> dmns.recommend_exhibits(patron_1)
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
-#
-# pry(main)> dmns.recommend_exhibits(patron_2)
-# # => [#<Exhibit:0x00007fb400acc590...>]
-# ```
